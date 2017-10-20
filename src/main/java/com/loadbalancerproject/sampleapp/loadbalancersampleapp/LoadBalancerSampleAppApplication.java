e4042348e633cbaba4b4841843c7ff1a43feb856
Mateusz Kuzmik <mkuzmik@akamai.com>
2017-10-20 16:39:03 +0200

    Initial commit

---
+++ src/main/java/com/loadbalancerproject/sampleapp/loadbalancersampleapp/LoadBalancerSampleAppApplication.java
@@ -0,0 +1,12 @@
+package com.loadbalancerproject.sampleapp.loadbalancersampleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadBalancerSampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadBalancerSampleAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/loadbalancerproject/sampleapp/loadbalancersampleapp/LoadBalancerSampleAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.loadbalancerproject.sampleapp.loadbalancersampleapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoadBalancerSampleAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
