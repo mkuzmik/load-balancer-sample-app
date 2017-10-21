e3a225d65510ea4757b5e0374672bb6f48a3d83b
Mateusz Kuzmik <mkuzmik@akamai.com>
2017-10-21 12:47:44 +0200

    Dependecies changes. Sample controller created.

--- src/main/java/com/loadbalancerproject/sampleapp/loadbalancersampleapp/LoadBalancerSampleAppApplication.java
+++ src/main/java/com/loadbalancerproject/sampleapp/loadbalancersampleapp/LoadBalancerSampleAppApplication.java
@@ -2,9 +2,20 @@ package com.loadbalancerproject.sampleapp.loadbalancersampleapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class LoadBalancerSampleAppApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String main() {
+		return "Hello";
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoadBalancerSampleAppApplication.class, args);
