f76ed8d49d4569d1102efad3f4befbb127e36b4c
Mateusz Kuzmik <mkuzmik@akamai.com>
2017-11-09 18:17:40 +0100

    Repository refactored.

--- src/main/java/com/loadbalancerproject/sampleapp/loadbalancersampleapp/student/StudentRepository.java
+++ src/main/java/com/loadbalancerproject/sampleapp/loadbalancersampleapp/student/StudentRepository.java
@@ -1,30 +1,52 @@
 package com.loadbalancerproject.sampleapp.loadbalancersampleapp.student;
 
 import com.loadbalancerproject.sampleapp.loadbalancersampleapp.student.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transaction;
 import java.util.List;
 
 @Repository
-@Transactional
 public class StudentRepository {
 
-    @PersistenceContext
-    EntityManager entityManager;
+    private EntityManagerFactory entityManagerFactory;
+    private EntityManager entityManager;
+
+    @Autowired
+    public StudentRepository(EntityManagerFactory entityManagerFactory) {
+        this.entityManagerFactory = entityManagerFactory;
+    }
+
+    private void setup() {
+        entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+    }
+
+    private void commitAndClose() {
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
 
     public void save(Student student) {
+        setup();
         entityManager.persist(student);
+        commitAndClose();
     }
 
     public void delete(Student student) {
+        setup();
         Student merged = entityManager.merge(student);
         entityManager.remove(merged);
+        commitAndClose();
     }
 
     public List<Student> getAll() {
+        entityManager = entityManagerFactory.createEntityManager();
         return (List<Student>) entityManager.createQuery("from Student").getResultList();
     }
 }
